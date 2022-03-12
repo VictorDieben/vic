caac389ec86d2f5b10e491811b1599a18c1c312f
JorkStapel <jork.stapel@hotmail.com>
2022-03-12 20:45:07 +0100

    added .h and.cpp in root to work with Arduino IDE (not yet working at all)

---
+++ vic.cpp
@@ -0,0 +1,14 @@
+/*
+  Morse.cpp - Defines the functions for the static library.
+*/
+#include "Arduino.h"
+#include "vic.h"
+
+/*
+
+//__vic__::fnvic() { }
+
+void __vic__::fnvic(){
+	delay(250);
+}
+*/
---
+++ vic.h
@@ -0,0 +1,29 @@
+/*!
+ *  @file vic.h
+ *
+ *  This is a library for runtime-efficient robot kinematics and transforms.
+ *
+ *  
+ */
+
+// wrapper to prevent including multiple times 
+#ifndef vic_h
+#define vic_h
+
+#include "Arduino.h"
+
+#include "vic/utils.h"
+#include "vic/Emulator.h"
+#include "vic/EntitySystem.h"
+#include "vic/EventQueue.h"
+#include "vic/Graph.h"
+//#include "vic/pch.h" todo: can't find framework.h
+/*
+class __vic__{
+	public:
+	    void fnvic();
+	//Private:
+	
+};
+*/
+#endif
