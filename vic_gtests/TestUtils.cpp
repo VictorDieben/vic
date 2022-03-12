a5cd20ad395fa69e7e6ba3532b92bb1154b6df15
VictorDieben <victordieben@gmail.com>
2022-03-12 23:18:54 +0100

    * wip

--- vic_gtests/TestUtils.cpp
+++ vic_gtests/TestUtils.cpp
@@ -9,3 +9,10 @@ TEST(TestUtils, Startup)
     constexpr auto res = Pow<2>(3);
     EXPECT_EQ(res, 9);
 }
+
+TEST(TestUtils, TestTimer)
+{
+    CTimer timer;
+    timer.Reset();
+    std::chrono::duration interval = timer.GetTime();
+}
