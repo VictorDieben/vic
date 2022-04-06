72ac3c043396bb48f010352287025cfc246060f8
VictorDieben <victordieben@gmail.com>
2022-04-06 21:34:35 +0200

    * exponential rotation/transform wip

--- vic/kinematics/kinematics.h
+++ vic/kinematics/kinematics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vic/linalg/linalg.h"
+#include "vic/linalg/tools.h"
 
 namespace vic
 {
@@ -27,8 +28,14 @@ public:
         : mVector(vec)
     { }
 
-    Vector3<DataType> GetAngular() const { return {}; }
-    Vector3<DataType> GetLinear() const { return {}; }
+    Vector3<DataType> GetAngular() const
+    {
+        return {}; // todo
+    }
+    Vector3<DataType> GetLinear() const
+    {
+        return {}; // todo
+    }
 
 private:
     Vector6<DataType> mVector{};
@@ -37,6 +44,45 @@ private:
 // todo: separate types? they will be exactly the same
 using Twist = Screw;
 using Wrench = Screw;
+
+Matrix<DataType, 3, 3> ExponentialRotationHelper(const Matrix3<DataType>& b, //
+                                                 const Matrix3<DataType>& bSquared, //
+                                                 const DataType theta)
+{
+    return Add(Identity<DataType, 3>{}, //
+               Matmul(std::sin(theta), b), //
+               Matmul(1 - std::cos(theta), bSquared));
+}
+
+Matrix<DataType, 3, 3> ExponentialRotation(const Vector3<DataType>& axis, //
+                                           const DataType theta)
+{
+    const auto b = Matrix3<DataType>{Bracket3(axis)};
+    const auto bSquared = Matmul(b, b);
+    // todo: bracket * bracket is itself a bracket (i think)
+    return ExponentialRotationHelper(b, bSquared, theta);
+}
+
+Matrix<DataType, 4, 4> ExponentialTransform(const Screw& screw, //
+                                            const DataType theta)
+{
+    const auto angular = screw.GetAngular();
+    const auto linear = screw.GetLinear();
+    const auto b = Matrix3<DataType>{Bracket3(angular)}; // todo: Bracket3 overload for matmul
+    const auto bSquared = Matmul(b, b);
+
+    const auto tmp = Add(Identity<DataType, 3>{}, //
+                         Matmul(1. - std::cos(theta), b), //
+                         Matmul(theta - std::sin(theta), bSquared));
+
+    const auto rot = ExponentialRotationHelper(b, bSquared, theta);
+
+    Matrix<DataType, 4, 4> res{};
+    // todo: assign rot to [0:3, 0:3]
+    // todo: assign tmp*lin to [0:3, 3]
+    // todo: assing 1 to [3, 3]
+    return res;
+}
 
 } // namespace kinematics
 } // namespace vic
