8a23ac0a227dd035b47088d5a1da30a43b80a78a
JorkStapel <jork.stapel@hotmail.com>
2022-09-30 23:31:57 +0200

    Fixes compile error

--- vic/kinematics/rotation.h
+++ vic/kinematics/rotation.h
@@ -64,6 +64,7 @@ constexpr Matrix<T, 3, 3> EulerAngles(const T alpha, const T beta, const T gamma
     return Matmul(Rotate(xAxis, alpha), Rotate(yAxis, beta), Rotate(zAxis, gamma)); // intrinsic x->y'->z''
 }
 
+
 template <typename T>
 constexpr Matrix<T, 3, 3> Quaternion( T w,  T x, const T y, const T z)
 {
@@ -89,6 +90,8 @@ constexpr Matrix<T, 3, 3> Quaternion(std::vector<T> wxyz)
     return Quaternion(wxyz[0], wxyz[1], wxyz[2], wxyz[3]);
 }
 
+
+
 template <typename T>
 constexpr std::vector<T> R2Q(Matrix<T, 3, 3> R){
     std::vector<T> wxyz;
@@ -101,23 +104,24 @@ constexpr std::vector<T> R2Q(Matrix<T, 3, 3> R){
         wxyz[3] = (R.Get(1,0) - R.Get(0,1)) * s;
     } else{
         if ( R.Get(0,0) > R.Get(1,1) && R.Get(0,0) > R.Get(2,2) ) {
-      T s = 2.0 * sqrt( 1.0 + R.Get(0,0) - R.Get(1,1) - R.Get(2,2));
-      wxyz[0] = (R.Get(2,1) - R.Get(1,2) ) / s;
-      wxyz[1] = 0.25 * s;
-      wxyz[2] = (R.Get(0,1) + R.Get(1,0) ) / s;
-      wxyz[3] = (R.Get(0,2) + R.Get(2,0) ) / s;
-    } else if (R.Get(1,1) > R.Get(2,2)) {
-      TIMER_ALARM0_LSB s = 2.0 * sqrt( 1.0 + R.Get(1,1) - R.Get(0,0) - R.Get(2,2));
-      wxyz[0]= (R.Get(0,2) - R.Get(2,0) ) / s;
-      wxyz[1] = (R.Get(0,1) + R.Get(1,0) ) / s;
-      wxyz[2] = 0.25 * s;
-      wxyz[3] = (R.Get(1,2) + R.Get(2,1) ) / s;
-    } else {
-      T s = 2.0 * sqrtf( 1.0 + R.Get(2,2) - R.Get(0,0) - R.Get(1,1) );
-      wxyz[0] = (R.Get(1,0) - R.Get(0,1) ) / s;
-      wxyz[1] = (R.Get(0,2) + R.Get(2,0) ) / s;
-      wxyz[2] = (R.Get(1,2) + R.Get(2,1) ) / s;
-      wxyz[3] = 0.25 * s;
+            T s = 2.0 * sqrt( 1.0 + R.Get(0,0) - R.Get(1,1) - R.Get(2,2));
+            wxyz[0] = (R.Get(2,1) - R.Get(1,2) ) / s;
+            wxyz[1] = 0.25 * s;
+            wxyz[2] = (R.Get(0,1) + R.Get(1,0) ) / s;
+            wxyz[3] = (R.Get(0,2) + R.Get(2,0) ) / s;
+        } else if (R.Get(1,1) > R.Get(2,2)) {
+            T s = 2.0 * sqrt( 1.0 + R.Get(1,1) - R.Get(0,0) - R.Get(2,2));
+            wxyz[0]= (R.Get(0,2) - R.Get(2,0) ) / s;
+            wxyz[1] = (R.Get(0,1) + R.Get(1,0) ) / s;
+            wxyz[2] = 0.25 * s;
+            wxyz[3] = (R.Get(1,2) + R.Get(2,1) ) / s;
+        } else {
+            T s = 2.0 * sqrt( 1.0 + R.Get(2,2) - R.Get(0,0) - R.Get(1,1) );
+            wxyz[0] = (R.Get(1,0) - R.Get(0,1) ) / s;
+            wxyz[1] = (R.Get(0,2) + R.Get(2,0) ) / s;
+            wxyz[2] = (R.Get(1,2) + R.Get(2,1) ) / s;
+            wxyz[3] = 0.25 * s;
+        }
     }
 
     return wxyz;
