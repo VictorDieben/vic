a49880f17ddf27f7c7f3eaf4cab2f169bd14feb4
VictorDieben <victordieben@gmail.com>
2023-07-03 12:39:50 +0200

    * small fix

--- vic/utils/file.h
+++ vic/utils/file.h
@@ -8,23 +8,15 @@
 
 namespace vic
 {
-
-template <typename T, auto UniqueLambda = []() {}>
-struct UniqueType : public T
-{
-    // UniqueLambda does nothing. But every instance of a lambda will get a unique type,
-    // and therefore every definition of a UniqueType<T> will be its own type.
-    // Useful for making unique mutex types.
-};
-
-std::vector<char> FileToCharVec(const std::string filePath)
+template <typename T>
+std::vector<T> FileToVec(const std::string& filePath)
 {
     std::ifstream file{filePath, std::ios::ate | std::ios::binary};
     if(!file.is_open())
         throw std::runtime_error("failed to open file: " + filePath);
 
     const auto fileSize = static_cast<std::size_t>(file.tellg());
-    std::vector<char> result(fileSize);
+    std::vector<T> result(fileSize);
 
     file.seekg(0);
     file.read(result.data(), fileSize);
