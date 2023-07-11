cd6939347ebf758be43227f3b76c7c59596f0f73
VictorDieben <victordieben@gmail.com>
2023-07-11 13:08:59 +0200

    * reverted fileToVec to non-template, made inline

--- vic/utils/file.h
+++ vic/utils/file.h
@@ -3,20 +3,24 @@
 #include <fstream>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 #include <vector>
+
+#include <filesystem>
 
 namespace vic
 {
-template <typename T>
-std::vector<T> FileToVec(const std::string& filePath)
+
+inline std::vector<char> FileToCharVec(const std::filesystem::path& filePath)
 {
+    if(!std::filesystem::is_regular_file(filePath))
+        throw std::runtime_error("file: \'" + filePath.generic_string() + "\' is not a regular file!");
+
     std::ifstream file{filePath, std::ios::ate | std::ios::binary};
     if(!file.is_open())
-        throw std::runtime_error("failed to open file: " + filePath);
+        throw std::runtime_error("failed to open file: " + filePath.generic_string());
 
     const auto fileSize = static_cast<std::size_t>(file.tellg());
-    std::vector<T> result(fileSize);
+    std::vector<char> result(fileSize);
 
     file.seekg(0);
     file.read(result.data(), fileSize);
