bf864ba9a9d215a412c8436b4c56bfd1f4631a04
VictorDieben <victordieben@gmail.com>
2023-10-10 13:26:56 +0200

    * compile fix

--- vic/graph2/algorithms/mstar.h
+++ vic/graph2/algorithms/mstar.h
@@ -25,118 +25,118 @@ inline constexpr bool NthBitIsSet(const CollisionSet collisionset, const uint8_t
     const CollisionSet mask = ((uint64_t)1) << index;
     return collisionset & mask;
 }
-
-template <typename TGraph>
-class CartesianOutIterator
-{
-public:
-    using VertexIdType = typename TGraph::VertexIdType;
-
-    using TensorGraphType = typename TTensorGraph;
-    using GraphType = typename TTensorGraph::GraphType;
-
-private:
-    CartesianOutIterator& mGraph;
-    OutVertexIterator<GraphType> mOutIterator;
-
-public:
-    CartesianOutIterator(TTensorGraph& graph)
-        : mGraph(graph)
-        , mOutIterator(graph.GetGraph())
-    { }
-
-    void Update()
-    {
-        mOutIterator.Update(); //
-    }
-
-    template <typename TFunctor>
-    void ForeachOut(const VertexIdType id, TFunctor functor) const
-    {
-        ForeachOut(TensorVertexType(mGraph, id), functor);
-    }
-
-    template <typename TFunctor>
-    void ForeachOut(const TensorVertexType& vert, TFunctor functor) const
-    {
-        TensorVertexType copy = vert;
-        ForeachOutRecursive(copy, functor, 0, mGraph.GetDimensions());
-    }
-
-    template <typename TFunctor>
-    void ForeachValidOut(const TensorVertexId id, TFunctor functor) const
-    {
-        ForeachValidOut(TensorVertexType(mGraph, id), functor);
-    }
-
-    template <typename TFunctor>
-    void ForeachValidOut(const TensorVertexType& vert, TFunctor functor) const
-    {
-        TensorVertexType copy = vert;
-        std::unordered_set<VertexIdType> occupiedVertices(copy.GetVertices().begin(), copy.GetVertices().end());
-        if(occupiedVertices.size() < copy.GetVertices().size())
-            return;
-        ForeachValidOutRecursive(copy, functor, 0, mGraph.GetDimensions(), occupiedVertices);
-    }
-
-private:
-    template <typename TFunctor>
-    void ForeachOutRecursive(TensorVertexType& vertex,
-                             TFunctor functor, //
-                             const std::size_t dim,
-                             const std::size_t dims) const
-    {
-        if(dim == dims)
-        {
-            functor(vertex);
-            return;
-        }
-        const VertexIdType vertexAtDim = vertex.At(dim);
-
-        // loop over case where this dimension is constant
-        ForeachOutRecursive(vertex, functor, dim + 1, dims);
-
-        // todo: loop over all out vertices for this dimension
-        const auto& outVerts = mOutIterator.OutVertices(vertex.At(dim));
-        for(const auto& outVert : outVerts)
-        {
-            vertex.Set(dim, outVert);
-            ForeachOutRecursive(vertex, functor, dim + 1, dims);
-        }
-        vertex.Set(dim, vertexAtDim);
-    }
-
-    template <typename TFunctor>
-    void ForeachValidOutRecursive(TensorVertexType& vertex,
-                                  TFunctor functor, //
-                                  const std::size_t dim,
-                                  const std::size_t dims,
-                                  std::unordered_set<VertexIdType>& occupiedVertices) const
-    {
-        if(dim == dims)
-        {
-            functor(vertex);
-            return;
-        }
-        const VertexIdType vertexAtDim = vertex.At(dim);
-
-        // loop over case where this dimension is constant
-        ForeachValidOutRecursive(vertex, functor, dim + 1, dims, occupiedVertices);
-
-        // loop over all out vertices for this dimension
-        const auto& outVerts = mOutIterator.OutVertices(vertex.At(dim));
-        for(const auto& outVert : outVerts)
-        {
-            if(occupiedVertices.insert(outVert).second)
-            {
-                vertex.Set(dim, outVert);
-                ForeachValidOutRecursive(vertex, functor, dim + 1, dims, occupiedVertices);
-                occupiedVertices.erase(outVert);
-            }
-        }
-        vertex.Set(dim, vertexAtDim);
-    }
-};
+//
+//template <typename TGraph>
+//class CartesianOutIterator
+//{
+//public:
+//    using VertexIdType = typename TGraph::VertexIdType;
+//
+//    using TensorGraphType = typename TTensorGraph;
+//    using GraphType = typename TTensorGraph::GraphType;
+//
+//private:
+//    CartesianOutIterator& mGraph;
+//    OutVertexIterator<GraphType> mOutIterator;
+//
+//public:
+//    CartesianOutIterator(TTensorGraph& graph)
+//        : mGraph(graph)
+//        , mOutIterator(graph.GetGraph())
+//    { }
+//
+//    void Update()
+//    {
+//        mOutIterator.Update(); //
+//    }
+//
+//    template <typename TFunctor>
+//    void ForeachOut(const VertexIdType id, TFunctor functor) const
+//    {
+//        ForeachOut(TensorVertexType(mGraph, id), functor);
+//    }
+//
+//    template <typename TFunctor>
+//    void ForeachOut(const TensorVertexType& vert, TFunctor functor) const
+//    {
+//        TensorVertexType copy = vert;
+//        ForeachOutRecursive(copy, functor, 0, mGraph.GetDimensions());
+//    }
+//
+//    template <typename TFunctor>
+//    void ForeachValidOut(const TensorVertexId id, TFunctor functor) const
+//    {
+//        ForeachValidOut(TensorVertexType(mGraph, id), functor);
+//    }
+//
+//    template <typename TFunctor>
+//    void ForeachValidOut(const TensorVertexType& vert, TFunctor functor) const
+//    {
+//        TensorVertexType copy = vert;
+//        std::unordered_set<VertexIdType> occupiedVertices(copy.GetVertices().begin(), copy.GetVertices().end());
+//        if(occupiedVertices.size() < copy.GetVertices().size())
+//            return;
+//        ForeachValidOutRecursive(copy, functor, 0, mGraph.GetDimensions(), occupiedVertices);
+//    }
+//
+//private:
+//    template <typename TFunctor>
+//    void ForeachOutRecursive(TensorVertexType& vertex,
+//                             TFunctor functor, //
+//                             const std::size_t dim,
+//                             const std::size_t dims) const
+//    {
+//        if(dim == dims)
+//        {
+//            functor(vertex);
+//            return;
+//        }
+//        const VertexIdType vertexAtDim = vertex.At(dim);
+//
+//        // loop over case where this dimension is constant
+//        ForeachOutRecursive(vertex, functor, dim + 1, dims);
+//
+//        // todo: loop over all out vertices for this dimension
+//        const auto& outVerts = mOutIterator.OutVertices(vertex.At(dim));
+//        for(const auto& outVert : outVerts)
+//        {
+//            vertex.Set(dim, outVert);
+//            ForeachOutRecursive(vertex, functor, dim + 1, dims);
+//        }
+//        vertex.Set(dim, vertexAtDim);
+//    }
+//
+//    template <typename TFunctor>
+//    void ForeachValidOutRecursive(TensorVertexType& vertex,
+//                                  TFunctor functor, //
+//                                  const std::size_t dim,
+//                                  const std::size_t dims,
+//                                  std::unordered_set<VertexIdType>& occupiedVertices) const
+//    {
+//        if(dim == dims)
+//        {
+//            functor(vertex);
+//            return;
+//        }
+//        const VertexIdType vertexAtDim = vertex.At(dim);
+//
+//        // loop over case where this dimension is constant
+//        ForeachValidOutRecursive(vertex, functor, dim + 1, dims, occupiedVertices);
+//
+//        // loop over all out vertices for this dimension
+//        const auto& outVerts = mOutIterator.OutVertices(vertex.At(dim));
+//        for(const auto& outVert : outVerts)
+//        {
+//            if(occupiedVertices.insert(outVert).second)
+//            {
+//                vertex.Set(dim, outVert);
+//                ForeachValidOutRecursive(vertex, functor, dim + 1, dims, occupiedVertices);
+//                occupiedVertices.erase(outVert);
+//            }
+//        }
+//        vertex.Set(dim, vertexAtDim);
+//    }
+//};
 
 template <typename TGraph, typename TEdgeCostFunctor, typename THeuristicFunctor>
 auto MStar(const TGraph& graph, //
